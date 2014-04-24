7c0315ae4952543bcd3a8e00086a2e2e12c18466
Peter Bronkalla <peterbronkalla@googlemail.com>
2014-04-24 17:20:39 +0200

    Added validate function for user input for challenge 159

--- challenge-159/RPSLP.js
+++ challenge-159/RPSLP.js
@@ -7,10 +7,24 @@ var rl = readline.createInterface({
 
 var possibilities = ["rock", "paper", "scissors", "lizard", "spock"];
 
+var validateChoice = function(userChoice) {
+    var validated = false;
+    for (var i = 0; i < possibilities.length; i++) {
+        if (userChoice === possibilities[i])
+            validated = true;
+    }
+
+    return validated;
+}
+
 rl.question("Your input for the game: ", function(answer) {
-    var computerChoice = Math.floor(Math.random()*4);
-	console.log("Your choice was: " + answer);
-    console.log("Computer choice was: " + possibilities[computerChoice]);
+    if (validateChoice(answer)) {
+        var computerChoice = Math.floor(Math.random()*4);
+        console.log("Your choice was: " + answer);
+        console.log("Computer choice was: " + possibilities[computerChoice]);
+    } else {
+        console.log("You entered the wrong input. Try again!");
+    }
 
 
 	rl.close();
